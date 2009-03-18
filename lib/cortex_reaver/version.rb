5735bbb8db28e831fad5e5699073de4d994b6d14
Aphyr <aphyr@unstable.(none)>
2009-03-17 23:02:13 -0500

    Version Bump

--- lib/cortex_reaver/version.rb
+++ lib/cortex_reaver/version.rb
@@ -1,8 +1,8 @@
 module CortexReaver
   APP_NAME = 'Cortex Reaver'
-  APP_VERSION = '0.0.6'
+  APP_VERSION = '0.0.7'
   APP_AUTHOR = 'aphyr'
   APP_EMAIL = 'aphyr@aphyr.com'
   APP_URL = 'http://aphyr.com'
-  APP_COPYRIGHT = 'Copyright (c) 2008 aphyr <aphyr@aphyr.com>. All rights reserved.'
+  APP_COPYRIGHT = 'Copyright (c) 2009 aphyr <aphyr@aphyr.com>. All rights reserved.'
 end
