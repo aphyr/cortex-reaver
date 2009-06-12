9b786d0b7747b30f6448e2eb48c93a89718f996b
Kyle Kingsbury <aphyr@aphyr.com>
2009-06-12 14:05:07 -0500

    Spec testing

--- spec/main.rb
+++ spec/main.rb
@@ -7,11 +7,26 @@ begin
   bin = "#{File.dirname(__FILE__)}/../bin/cortex_reaver"
   root = "/tmp/cortex-reaver-test-#{Process.pid}"
   Dir.chdir root
-  `bin --migrate`
-  `bin --start`
+  `#{bin} --migrate`
+  `#{bin} --start`
   
-  describe MainController do
+  describe 'Main Controller' do
     behaves_like 'http', 'xpath'
+
+    def check_page(name = '')
+      page = get("/#{name}")
+      page.status.should == 200
+      page.body.should.not == nil
+
+      doc = Hpricot(page.body)
+      doc.at('title').inner_html.should == 'Blog'
+      doc.at('h1').inner_html.should == 'Blog'
+
+      doc.search('div#entries').size.should == 1
+
+      doc
+    end
+
     it 'should show main page' do
       got = get('/')
       got.status.should == 200
