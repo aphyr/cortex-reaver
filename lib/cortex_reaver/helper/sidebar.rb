c3f3238bc67c7756d5200e6beccd71948d3a428d
Aphyr <aphyr@waterhouse.(none)>
2009-07-27 20:29:33 -0500

    Added sidebar support files.

---
+++ lib/cortex_reaver/helper/sidebar.rb
@@ -0,0 +1,44 @@
+module Ramaze
+  module Helper
+    # Provides programmatic sidebar boxes
+    module Sidebar
+      # Renders the sidebar
+      def sidebar
+        sidebar = ''
+        CortexReaver.config.view.sidebar.each do |path, view|
+          if path_matches? path
+            sidebar << CortexReaver::MainController.render_view('sidebar/' + view)
+          end
+        end
+        sidebar
+      end
+
+      # True if the given glob matches our request path.
+      def path_matches?(path)
+        case path
+        when String
+          # Convert string to regex.
+          pattern = path.gsub(/\*(\*)?/) do |match|
+            if match[1]
+              '.*'
+            else
+              '[^\/]*'
+            end
+          end
+
+          if Regexp.new("^#{pattern}$").match request.path_info
+            true
+          else
+            false
+          end
+        when Regexp
+          if path.match request.path_info
+            true
+          else
+            false
+          end
+        end
+      end
+    end
+  end
+end
