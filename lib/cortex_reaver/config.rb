f8fb499a351e89d9d656e190976c1d2654dc72b0
Kyle Kingsbury <kyle@vodpod.com>
2009-09-03 10:36:46 -0700

    Fixed earliest-date computation.
    When there were no entries for a class, the earliest date could not be
    displayed.

--- lib/cortex_reaver/config.rb
+++ lib/cortex_reaver/config.rb
@@ -177,13 +177,14 @@ You can also just provide a regex for the path, in which case it is matched dire
     def earliest_content
       return @earliest_content if @earliest_content
 
+      ej = CortexReaver::Journal.dataset.min(:created_on)
       @earliest_content = DateTime.parse(
         [
           CortexReaver::Journal.dataset.min(:created_on),
           CortexReaver::Page.dataset.min(:created_on),
           CortexReaver::Photograph.dataset.min(:created_on),
           CortexReaver::Project.dataset.min(:created_on)
-        ].min.to_s
+        ].compact.min.to_s
       )
     end
 
