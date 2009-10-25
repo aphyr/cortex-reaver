2712732ce8e8deb8cebbd78b5f884610ceff0815
Kyle Kingsbury <aphyr@aphyr.com>
2009-10-25 12:12:04 -0700

    Updated memcached snippet for 2009.07.

--- lib/cortex_reaver/snippets/ramaze/cache/memcached.rb
+++ lib/cortex_reaver/snippets/ramaze/cache/memcached.rb
@@ -1,14 +1,17 @@
 module Ramaze
-  class MemcachedCache
-    # I can't figure out a good way to specify the parameters Ramaze::Cache
-    # uses when instantiating new caches, so I'm just going to override the
-    # setup here. Innate hardcodes the app name as "pristine". :(
-    def cache_setup(host, user, app, name)
-      app = Ramaze.options.app.name
-      @namespace = [host, user, app, name].compact.join('-')
-      options = {:namespace => @namespace}.merge(OPTIONS)
-      servers = options.delete(:servers)
-      @store = ::MemCache.new(servers, options)
+  class Cache
+    class MemCache
+      # I can't figure out a good way to specify the parameters Ramaze::Cache
+      # uses when instantiating new caches, so I'm just going to override the
+      # setup here. Innate hardcodes the app name as "pristine". :(
+      def cache_setup(host, user, app, name)
+        app = Ramaze.options.app.name
+        @namespace = [host, user, app, name].compact.join('-')
+        options = {:namespace => @namespace}.merge(OPTIONS)
+        servers = options.delete(:servers)
+        @store = ::MemCache.new(servers, options)
+        @warned = false
+      end
     end
   end
 end
