5f15876ca1f61c06ea1cb3fb1a19084b3c8c5969
Aphyr <aphyr@aphyr.com>
2009-12-13 22:56:27 -0800

    Version 0.2.4

--- lib/cortex_reaver/version.rb
+++ lib/cortex_reaver/version.rb
@@ -1,6 +1,6 @@
 module CortexReaver
   APP_NAME = 'Cortex Reaver'
-  APP_VERSION = '0.2.3'
+  APP_VERSION = '0.2.4'
   APP_AUTHOR = 'Kyle Kingsbury'
   APP_EMAIL = 'aphyr@aphyr.com'
   APP_URL = 'http://aphyr.com'
