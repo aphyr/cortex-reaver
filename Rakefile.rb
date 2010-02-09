acb3dd40125d1c595ab3505705f0067a0947c058
Aphyr <aphyr@aphyr.com>
2010-02-09 11:37:54 -0800

    Whoops, added Trollop to the gemspec.

--- Rakefile.rb
+++ Rakefile.rb
@@ -42,6 +42,7 @@ cortex_reaver_gemspec = Gem::Specification.new do |s|
   s.add_dependency('cssmin', '~>1.0.2')
   s.add_dependency('jsmin', '~>1.0.1')
   s.add_dependency('json', '~>1.1.9')
+  s.add_dependency('trollop', '~>1.15')
 end
  
 Rake::GemPackageTask.new(cortex_reaver_gemspec) do |p|
